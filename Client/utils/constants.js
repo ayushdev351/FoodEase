11f29c201a07723f71888eaed4680979c52985ad
ayushdev351 <mrayushchauhan351@gmail.com>
2023-08-09 01:59:10 +0530

    Fixed CORS error

--- Client/utils/constants.js
+++ Client/utils/constants.js
@@ -1,10 +1,10 @@
 export const LOGO_URL = "https://icon-library.com/images/order-food-online-icon/order-food-online-icon-5.jpg";
 
-export const Cloudinary_URL = "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+export const Cloudinary_URL = "https://corsproxy.io/?https%3A%2F%2Fres.cloudinary.com%2Fswiggy%2Fimage%2Fupload%2Ffl_lossy%2Cf_auto%2Cq_auto%2Cw_508%2Ch_320%2Cc_fill%2F";
 
-export const RES_API_URL = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.642996778080303&lng=77.37636666744947&page_type=DESKTOP_WEB_LISTING";
+export const RES_API_URL = "https://corsproxy.io/?https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Frestaurants%2Flist%2Fv5%3Flat%3D28.642996778080303%26lng%3D77.37636666744947%26page_type%3DDESKTOP_WEB_LISTING";
 
 // For seperate Restaurant
-export const RES_INFO_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.642996778080303&lng=77.37636666744947&restaurantId="
+export const RES_INFO_API = "https://corsproxy.io/?https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Fmenu%2Fpl%3Fpage-type%3DREGULAR_MENU%26complete-menu%3Dtrue%26lat%3D28.642996778080303%26lng%3D77.37636666744947%26restaurantId%3D"
 
-export const DISH_IMG_URL = "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/"
+export const DISH_IMG_URL = "https://corsproxy.io/?https%3A%2F%2Fres.cloudinary.com%2Fswiggy%2Fimage%2Fupload%2Ffl_lossy%2Cf_auto%2Cq_auto%2Cw_208%2Ch_208%2Cc_fit%2F"
