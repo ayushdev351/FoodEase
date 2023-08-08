39432b9779fea3d1abd4ff67e193408578960570
ayushdev351 <mrayushchauhan351@gmail.com>
2023-08-09 00:54:53 +0530

    Grouping tests

--- Client/components/__tests__/help.test.js
+++ Client/components/__tests__/help.test.js
@@ -3,40 +3,42 @@ import "@testing-library/jest-dom";
 
 import Help from "../Help"
 
-test("Should load Help component", () => {
-    render(<Help />);
-
-    const heading = screen.getByRole("heading");
-
-    expect(heading).toBeInTheDocument();
-})
-
-test("Should Load button inside Help component", () => {
-    render(<Help/>);
-
-    const button = screen.getByRole("button");
-
-    // Actual virtual dom node
-    // console.log(button)
-
-    expect(button).toBeInTheDocument();
-})
-
-test("Should contain text 'you' inside Help Component", () => {
-    render(<Help/>);
-
-    // Element with text -> 'you'
-    const text = screen.getByText("you");
-
-    expect(text).toBeInTheDocument();
-})
-
-test("Should contain 2 input boxes in Help component", () => {
-    render(<Help/>);
-
-    const inputs = screen.getAllByRole('textbox');
-
-    // console.log(inputs)
-
-    expect(inputs.length).toBe(2);
+describe("Help page test cases", () => {
+    test("Should load Help component", () => {
+        render(<Help />);
+    
+        const heading = screen.getByRole("heading");
+    
+        expect(heading).toBeInTheDocument();
+    })
+    
+    test("Should Load button inside Help component", () => {
+        render(<Help/>);
+    
+        const button = screen.getByRole("button");
+    
+        // Actual virtual dom node
+        // console.log(button)
+    
+        expect(button).toBeInTheDocument();
+    })
+    
+    test("Should contain text 'you' inside Help Component", () => {
+        render(<Help/>);
+    
+        // Element with text -> 'you'
+        const text = screen.getByText("you");
+    
+        expect(text).toBeInTheDocument();
+    })
+    
+    test("Should contain 2 input boxes in Help component", () => {
+        render(<Help/>);
+    
+        const inputs = screen.getAllByRole('textbox');
+    
+        // console.log(inputs)
+    
+        expect(inputs.length).toBe(2);
+    })
 })
