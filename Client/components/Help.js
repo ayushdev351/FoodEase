878730171c4157e955e7bd44617b0838007a9c65
ayushdev351 <mrayushchauhan351@gmail.com>
2023-08-09 00:47:26 +0530

    Add basic ui unit tests for help page

--- Client/components/Help.js
+++ Client/components/Help.js
@@ -1,10 +1,12 @@
 const Help = () => {
     return(
         <div>
-            <h1>Help Page Heading</h1>
+            <h1>Help Page Heading : Helping Hand</h1>
+            <p>We are here to help you</p>
+            <p>you</p>
             <form>
                 <input type = "text"></input>
-                <input type = "password"></input>
+                <input type = "text"></input>
                 <button type = "submit">submit</button>
             </form>
         </div>
--- Client/components/__tests__/help.test.js
+++ Client/components/__tests__/help.test.js
@@ -10,3 +10,33 @@ test("Should load Help component", () => {
 
     expect(heading).toBeInTheDocument();
 })
+
+test("Should Load button inside Help component", () => {
+    render(<Help/>);
+
+    const button = screen.getByRole("button");
+
+    // Actual virtual dom node
+    // console.log(button)
+
+    expect(button).toBeInTheDocument();
+})
+
+test("Should contain text 'you' inside Help Component", () => {
+    render(<Help/>);
+
+    // Element with text -> 'you'
+    const text = screen.getByText("you");
+
+    expect(text).toBeInTheDocument();
+})
+
+test("Should contain 2 input boxes in Help component", () => {
+    render(<Help/>);
+
+    const inputs = screen.getAllByRole('textbox');
+
+    // console.log(inputs)
+
+    expect(inputs.length).toBe(2);
+})
