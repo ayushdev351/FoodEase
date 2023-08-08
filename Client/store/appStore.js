90526e61e1b298ddb6e067a9b2dcd24cfe3c55bf
ayushdev351 <mrayushchauhan351@gmail.com>
2023-08-09 02:13:22 +0530

    removed error in importing cartSlice

--- Client/store/appStore.js
+++ Client/store/appStore.js
@@ -1,6 +1,6 @@
 import {configureStore} from "@reduxjs/toolkit"
 
-import cartReducer from "./cartSLice";
+import cartReducer from "./cartSlice";
 
 const appStore = configureStore({
     reducer : {
